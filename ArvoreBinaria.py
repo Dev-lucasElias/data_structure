c441c3181019240cc0a7e855f56805f529e80e4d
Lucas <developer.elias@outlook.com.br>
2024-05-12 21:49:49 -0300

    Iniciando implementação de arvores

---
+++ ArvoreBinaria.py
@@ -0,0 +1,7 @@
+from element.Node import Node
+
+class tree:
+      def __init__(self,node = None) -> None:
+            self.__root = node
+            
+      
---
+++ element/Node.py
@@ -0,0 +1,20 @@
+
+class Node:
+      def __init__(self, value) -> None:
+            self.__value = value
+            self.__left = None
+            self.__right = None
+      
+      def getLeft(self):
+            return self.__left
+      def getRight(self):
+            return self.__right
+      
+      def setLeft(self,novo):
+            self.__left = novo
+            
+      def setright(self,novo):
+            self.__right = novo
+      
+      def getValeu(self):
+            return self.__value
